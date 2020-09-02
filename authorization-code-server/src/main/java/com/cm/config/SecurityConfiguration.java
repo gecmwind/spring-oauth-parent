6c76daab73b8f93efcb86e7eb9bc74a41dc750df
gecm <gecmwind@163.com>
2020-09-02 23:28:49 +0800

    参考github例子的oauth2 授权码模式

--- authorization-code-server/src/main/java/com/cm/config/SecurityConfiguration.java
+++ authorization-code-server/src/main/java/com/cm/config/SecurityConfiguration.java
@@ -1,50 +1,75 @@
 package com.cm.config;
 
-import com.cm.service.impl.CustomDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomDetailsServiceImpl customDetailsService;
-    @Autowired
-    private ClientDetailsService clientDetailsService;
+
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
     @Override
-    @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customDetailsService).passwordEncoder(encoder());
+    protected UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        // 创建两个 qq 用户
+//        manager.createUser(User.withUsername("250577914").password(new BCryptPasswordEncoder().encode("123456")).authorities("USER").build());
+        manager.createUser(User.withUsername("250577914").password("123456").authorities("USER").build());
+        manager.createUser(User.withUsername("920129126").password(new BCryptPasswordEncoder().encode("123456")).authorities("USER").build());
+        return manager;
     }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.NEVER);
-    }
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring();
-    }
+
+
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+        AuthenticationManager manager = super.authenticationManagerBean();
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.
+            requestMatchers()
+                // /oauth/authorize link org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint
+                // 必须登录过的用户才可以进行 oauth2 的授权码申请
+                .antMatchers("/", "/home","/login","/oauth/authorize")
+                .and()
+            .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .and()
+            .httpBasic()
+                .disable()
+            .exceptionHandling()
+                .accessDeniedPage("/login?authorization_error=true")
+                .and()
+            // TODO: put CSRF protection back into this endpoint
+            .csrf()
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
+                .disable();
+//                .loginPage("/login")
+//                .failureUrl("/login?authentication_error=true")
+//        .httpBasic();
+        // @formatter:on
     }
 }
--- oauth-client/src/main/java/com/cm/OauthClientApplication.java
+++ oauth-client/src/main/java/com/cm/OauthClientApplication.java
@@ -1,12 +1,12 @@
 package com.cm;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author cm.ge.
- */
 @SpringBootApplication
+@RestController
 public class OauthClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(OauthClientApplication.class, args);
